1b85507b906c2fc0171423262353df9c88b7267e
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-18 21:28:15 +0000

    Ready basic app structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import css from './App.module.css';
-import Sidebar from "./components/Sidebar";
+import HTTPRequests from './components/HTTPRequests';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <Sidebar />
+      <HTTPRequests />
     </div>
   );
 }
