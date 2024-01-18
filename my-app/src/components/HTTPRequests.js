af958602b039362dbf3839b3b49e361ae3f75fbc
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-18 21:42:04 +0000

    Using axios fetch API data in class component and show it on screen

--- my-app/src/components/HTTPRequests.js
+++ my-app/src/components/HTTPRequests.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export class HTTPRequests extends Component {
     constructor(props) {
@@ -8,12 +9,23 @@ export class HTTPRequests extends Component {
          posts: []
       }
     }
+     
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            // console.log(response)
+            this.setState({
+                posts: response.data
+            })
+        })
+    }
   render() {
     return (
       <div>
         <h2>
             Posts:
         </h2>
+        {JSON.stringify(this.state.posts)}
       </div>
     )
   }
